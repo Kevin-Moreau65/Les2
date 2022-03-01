89802141b6dc82b884deecc3b733775f94ebf2ab
dinoz0 <78912997+dinoz0@users.noreply.github.com>
2022-03-01 14:45:12 +0100

    premier push (ouaiiiiiiiiiii)

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,21 +24,18 @@ const Home: NextPage = () => {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Villa 1 &rarr;</h2>
+            <img className={styles.img} src="https://i.pinimg.com/originals/f1/7a/09/f17a09db50bc69d3ae1708b1bf2ccb87.png"></img>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Villa 2 &rarr;</h2>
+            <img className={styles.img} src="https://i.pinimg.com/originals/f1/7a/09/f17a09db50bc69d3ae1708b1bf2ccb87.png"></img>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Villa 3 &rarr;</h2>
+            <img className={styles.img} src="https://i.pinimg.com/originals/f1/7a/09/f17a09db50bc69d3ae1708b1bf2ccb87.png"></img>
           </a>
 
           <a
