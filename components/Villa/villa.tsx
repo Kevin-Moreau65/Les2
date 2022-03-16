43e1b080cb28e6d97e987a9786496c1e4f5a3310
dinoz0 <78912997+dinoz0@users.noreply.github.com>
2022-03-16 18:30:34 +0100

    correction merge villa

--- components/Villa/villa.tsx
+++ components/Villa/villa.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import internal from 'stream';
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/Home.module.css';
+import stylesvilla from './villa.module.css';
 
 
 
@@ -19,11 +20,11 @@ export interface Props {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <img className={styles.img} src={props.photo}></img>
-            <div id={styles.divVilla}>
-              <h2 id={styles.Name}> {props.name}</h2>
-              <h2 id={styles.Prix}>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(props.prix)}</h2>
+            <div id={stylesvilla.divVilla}>
+              <h2 id={stylesvilla.Name}> {props.name}</h2>
+              <h2 id={stylesvilla.Prix}>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(props.prix)}</h2>
             </div>
-            <h2 id={styles.Adresse}>{props.adresse}</h2>            
+            <h2 id={stylesvilla.Adresse}>{props.adresse}</h2>            
           </a>
         </div>
       );
