76ec2ef7a44bf2550012b428652a8c985a8d514c
dinoz0 <78912997+dinoz0@users.noreply.github.com>
2022-04-25 09:36:11 +0200

    final push

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -2,30 +2,32 @@ import Link from 'next/link'
 import styles from './header.module.css'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-const checkConnection = () => {
-    const token = localStorage.getItem("token")
-    if (!token) return false
-    //appel API pour vérif le token
-    return token === "OK"
-}
-const Admin = () => {
-    const router = useRouter()
-    const logout = () => {
-        localStorage.removeItem("token")
-        router.push('/')
-    }
-    return <div className={styles.navigate}>
-        <Link href={'/admin'}>
-            Admin
-        </Link>
-        <Link href={'/admin/meet'}>
-            Réunions
-        </Link>
-        <p onClick={logout} style={{ cursor: "pointer" }}>
-            Se déconnecter
-        </p>
-    </div>
-}
+// const checkConnection = async () => {
+//         const token = localStorage.getItem("token")
+//         if (!token) return false
+//         const res = await fetch(`http://les2api.30d7d9e7b2014052a7b4.francecentral.aksapp.io/check`)
+//         const data = await res.json()
+//         if (data.message !== "OK") return false
+//         return true
+// }
+// const Admin = () => {
+//     const router = useRouter()
+//     const logout = () => {
+//         localStorage.removeItem("token")
+//         router.push('/')
+//     }
+//     return <div className={styles.navigate}>
+//         <Link href={'/admin'}>
+//             Admin
+//         </Link>
+//         <Link href={'/admin/meet'}>
+//             Réunions
+//         </Link>
+//         <p onClick={logout} style={{ cursor: "pointer" }}>
+//             Se déconnecter
+//         </p>
+//     </div>
+// }
 const Client = () => {
     return <div className={styles.navigate}>
         <Link href={'/login'}>
@@ -34,22 +36,25 @@ const Client = () => {
     </div>
 }
 const Header = () => {
-    const router = useRouter()
-    const [status, setStatus] = useState(false)
-    useEffect(() => {
-        router.events.on("routeChangeComplete", () => {
-            setStatus(checkConnection())
-        })
-        return router.events.off("routeChangeComplete", () => {
-        })
-    }, [router.events])
+    // const router = useRouter()
+    // const [status, setStatus] = useState(false)
+    // useEffect(() => {
+    //     router.events.on("routeChangeComplete", () => {
+    //         async function check() {
+    //             return setStatus(await checkConnection())
+    //         }
+    //        check() 
+    //     })
+    //     return router.events.off("routeChangeComplete", () => {
+    //     })
+    // }, [router.events])
     return <div className={styles.nav}>
         <div className={styles.navigate}>
             <Link href={'/'}>
                 Home
             </Link>
         </div>
-        {status ? <Admin /> : <Client />}
+        <Client/>
     </div>
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,6 @@ const Home = ({ articles }: props) => {
           <Villa id={"3"} name='Villa "Navarre"' prix={"1000€"} adresse="Pau, France" photo="https://media.abcsalles.com/images/1/salles/1440x960/508567/villa-navarre-7.jpg" />
           <Villa id={"2"} name='Villa "The One"' prix={"1000€"} adresse="Bel Air, Los Angeles" photo="https://4.bp.blogspot.com/-B6ruhrpXKbc/XxnadnE49qI/AAAAAAAB-FY/X_oEH6JHPb0M0TFnqK2WIizR0KvrHCsWwCLcBGAsYHQ/s1600-rw/23%2BPhotos%2Bvs.%2B%2524500%252C000%252C000%2B%2BBel%2BAir%2BMega%2BMansion%2B%2BLos%2BAngeles%2B-%2BLuxury%2BHome%2B%2526%2BInterior%2BDesign%2BTour%2B%25281%2529.PNG" />
           <Villa id={"3"} name='Villa "Navarre"' prix={"1000€"} adresse="Pau, France" photo="https://media.abcsalles.com/images/1/salles/1440x960/508567/villa-navarre-7.jpg" />
-          <Villa id={"1"} name='Villa "Stark"' prix={"1000€"} adresse="Malibu, California" photo="https://i.pinimg.com/originals/f1/7a/09/f17a09db50bc69d3ae1708b1bf2ccb87.png" />
           {articles.map((article) => (
             <Villa key={article._id} id={article._id} name={article.nom} prix={article.prix} photo={`http://les2api.30d7d9e7b2014052a7b4.francecentral.aksapp.io/media/${article.image}`} adresse={"test"}></Villa>
           ))}
